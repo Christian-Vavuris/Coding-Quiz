104eb47eb315c15368f3c64d12279fdcf08d289a
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-06-12 15:13:10 -0700

    added high scores page

---
+++ quiz.js
@@ -0,0 +1,36 @@
+var ButtonEl = document.querySelector("#startButton")
+
+
+
+
+
+// this function will remove the existing HTML in the quiz box
+var deleteTask = function() {
+  };
+
+// this function will decrease the timer by 1
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+
+
+
+
+
+
+ButtonEl.addEventListener("submit", startTimer );
