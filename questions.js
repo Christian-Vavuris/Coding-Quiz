cb9f47ff3a75c82adb548e79e40b1228944d2835
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-06-13 13:39:38 -0700

    added questions to a second JS file and figured out how to make stuff disappear

---
+++ questions.js
@@ -0,0 +1,42 @@
+var questions = [
+    {
+        question: "Which language is primarily used for styling a web page?",
+        answers: [
+            "HTML",
+            "JavaScript",
+            "CSS",
+            "Python",
+        ],
+        correctAnswer: "CSS",
+    },
+    {
+        question: "What HTML tag is used to describe the children of a <ul>",
+        answers: [
+            "<li>",
+            "<header>",
+            "<footer>",
+            "<article>",
+        ],
+        correctAnswer: "<li>",
+    },
+    {
+        question: "With display:flex enabled how do you set the vertical alignment of the child elements?",
+        answers: [
+            "justify-content",
+            "align-items",
+            "align-vertical",
+            "set-height",
+        ],
+        correctAnswer: "align-items",
+    },
+    {
+        question: "What is the correct HTML tag used when linking a stylesheet?",
+        answers: [
+            "<footer>",
+            "<stylesheet>",
+            "<link>",
+            "<script>",
+        ],
+        correctAnswer: "<script>",
+    }
+]
--- quiz.js
+++ quiz.js
@@ -1,36 +1,51 @@
-var ButtonEl = document.querySelector("#startButton")
+var buttonEl = document.querySelector("#startButton")
+
+//Need to add a timer
+//Need to add Quiz Questions
 
 
 
+// This function makes things go away
+var makeItGoAway = function (element) {
+    console.log("makeItGoAway is running")
+    document.querySelector(element).classList.add("hide")
+}
+// This is testing out the makeItGoAway
+// makeItGoAway("h1.quiz-title");
 
-
-// this function will remove the existing HTML in the quiz box
-var deleteTask = function() {
-  };
-
-// this function will decrease the timer by 1
-
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+var toggleTheHideFunction = function (selector, className) {
+    console.log("toggling classes");
 }
 
 
 
+// This function starts the clock at 60 and counts down one each time it is run.  It also only runs once a minute.
+
+var countDown = function () {
+    var clock = 60;
+    clock = clock - 1;
+    setInterval(countDown, 1000);
+    return clock;
+};
+
+var populateNextQuestion = function () {
+    let timer = 1;
+    console.lof(but)
+}
+
+//This function runs CountDown as long as the for loop is running
+
+var activeTimer = function () {
+    for (i = 0; i < 60; i++) {
+    countDown()
+    }
+}
+
+console.log(countDown());
+
+document.getElementById("timer").innerHTML = "Time Remaining: " + countDown();
 
 
 
 
-ButtonEl.addEventListener("submit", startTimer );
+// buttonEl.addEventListener("submit", startTimer);
