ea1902a066e83c00743dc484112faa1e30dec7fc
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-06-14 13:00:09 -0700

    added ending page

--- questions.js
+++ questions.js
@@ -1,42 +1,37 @@
+
+
+
+
 var questions = [
-    {
-        question: "Which language is primarily used for styling a web page?",
-        answers: [
-            "HTML",
-            "JavaScript",
-            "CSS",
-            "Python",
-        ],
-        correctAnswer: "CSS",
-    },
-    {
-        question: "What HTML tag is used to describe the children of a <ul>",
-        answers: [
-            "<li>",
-            "<header>",
-            "<footer>",
-            "<article>",
-        ],
-        correctAnswer: "<li>",
-    },
-    {
-        question: "With display:flex enabled how do you set the vertical alignment of the child elements?",
-        answers: [
-            "justify-content",
-            "align-items",
-            "align-vertical",
-            "set-height",
-        ],
-        correctAnswer: "align-items",
-    },
-    {
-        question: "What is the correct HTML tag used when linking a stylesheet?",
-        answers: [
-            "<footer>",
-            "<stylesheet>",
-            "<link>",
-            "<script>",
-        ],
-        correctAnswer: "<script>",
-    }
+    "Which language is primarily used for styling a web page?",
+    "What HTML tag is used to describe the children of a unlisted list item?",
+    "With display:flex enabled how do you set the vertical alignment of the child elements?",
+    "What is the correct HTML tag used when linking a javaScript file to your HTML?",
+]
+
+var choices = [
+    "HTML",
+    "JavaScript",
+    "CSS",
+    "Python",
+    "h1",
+    "footer",
+    "li",
+    "article",
+    "justify-content",
+    "align-vertical",
+    "align-items",
+    "set-height",
+    "footer",
+    "stylesheet",
+    "script",
+    "link",
+]
+
+
+var answers = [
+    "CSS",
+    "<li>",
+    "align-items",
+    "<script>",
 ]
--- quiz.js
+++ quiz.js
@@ -1,13 +1,21 @@
 // to do - 
-// style the CSS questions
-// 
+// Populate the quiz questions
+// Get the timer working
+// Add Local Storage and populate the high score page 
 
 var startButtonEl = document.getElementById("start-button")
 var startPageEl = document.getElementById("question-block")
+var buttonOneEl = document.getElementById("button-1")
+var buttonTwoEl = document.getElementById("button-2")
+var buttonThreeEl = document.getElementById("button-3")
+var buttonFourEl = document.getElementById("button-4")
+var inputNameEl = document.getElementById("submit-score")
 
-
-//Need to add a timer
-
+function clickHandler() {
+    clickHandler.counter++;
+    return clickHandler.counter;
+}
+clickHandler.counter = 0;
 
 
 // This function toggles the hide class onto the Element selected. 
@@ -15,12 +23,6 @@ var toggleHide = function (element) {
     console.log("toggleHide is running")
     document.getElementById(element).classList.toggle("hide")
 }
-
-var toggleHideClass = function (element) {
-    console.log("toggleHideClass is running")
-        document.getElementById(element).classList.toggle("hide")
-}
-
 
 // This function starts the clock at 60 and counts down one each time it is run.  It also only runs once a minute. It isn't wokring yet.
 
@@ -30,11 +32,6 @@ var countDown = function () {
     setInterval(countDown, 1000);
     return clock;
 };
-
-var populateNextQuestion = function () {
-    let timer = 1;
-    console.lof(but)
-}
 
 //This function runs CountDown as long as the for loop is running
 
@@ -55,9 +52,93 @@ startButtonEl.addEventListener("click", function() {
 });
 
 startButtonEl.addEventListener("click", function() {
-    toggleHideClass("question-block"), false
+    toggleHide("question-block"), false
     }
 );
+
+var displayEndPage = function () {
+    toggleHide("question-block"), false
+    toggleHide("end-page"), false
+}
+   
+
+// This sets up the first question. 
+
+startButtonEl.addEventListener("click", function () {
+    console.log(clickHandler());
+    document.getElementById("question-text").innerHTML = questions[0];
+    document.getElementById("button-1").innerHTML = choices[0];
+    document.getElementById("button-2").innerHTML = choices[1];
+    document.getElementById("button-3").innerHTML = choices[2];
+    document.getElementById("button-4").innerHTML = choices[3];
+});
+
+// This advances the quiz whent the user clicks the correct answer.  It also 
+
+var setQuestion = function () {
+
+    if (clickHandler.counter == 1) {
+        console.log(clickHandler());
+        document.getElementById("question-text").innerHTML = questions[1];
+        document.getElementById("button-1").innerHTML = choices[4];
+        document.getElementById("button-2").innerHTML = choices[5];
+        document.getElementById("button-3").innerHTML = choices[6];
+        document.getElementById("button-4").innerHTML = choices[7];
+        alert("Correct! The answer is " + answers[0])
+    }
+    else if (clickHandler.counter == 2) {
+        console.log(clickHandler());
+        document.getElementById("question-text").innerHTML = questions[2];
+        document.getElementById("button-1").innerHTML = choices[8];
+        document.getElementById("button-2").innerHTML = choices[9];
+        document.getElementById("button-3").innerHTML = choices[10];
+        document.getElementById("button-4").innerHTML = choices[11];
+        alert("Correct! The answer is " + answers[1])
+    }
+    else if (clickHandler.counter == 3) {
+        alert("Correct! The answer is " + answers[2])
+        console.log(clickHandler());
+        document.getElementById("question-text").innerHTML = questions[3];
+        document.getElementById("button-1").innerHTML = choices[12];
+        document.getElementById("button-2").innerHTML = choices[13];
+        document.getElementById("button-3").innerHTML = choices[14];
+        document.getElementById("button-4").innerHTML = choices[15];
+    }
+    else if (clickHandler.counter == 4) {
+        alert("Correct! The answer is " + answers[3])
+        console.log(clickHandler());
+        displayEndPage();
+    }
+    else {
+        displayEndPage()
+        console.log(clickHandler());
+    }
+};
+
+
+// This function defines the question that gets passed into the HTML 
+
+
+
+// This increases the counter each time the correct answer is selected 
+
+
+
+buttonOneEl.addEventListener("click", function() {
+    alert("Incorrect, please try again.")
+})
+buttonTwoEl.addEventListener("click", function() {
+    alert("Incorrect, please try again")
+})
+
+buttonThreeEl.addEventListener("click", function() {
+    setQuestion(); 
+});
+
+buttonFourEl.addEventListener("click", function() {
+    alert("Incorrect, please try again")
+})
+
 
 
 // buttonEl.addEventListener("submit", startTimer);
